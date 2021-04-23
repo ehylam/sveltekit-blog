57d212ff0ded9f7f4adbe6d92e9b252393caacee
Eric Lam <ehylam.p@gmail.com>
2021-04-23 14:39:05 +1000

    3D and HTML element merging tests

--- src/lib/threejs-playground.js
+++ src/lib/threejs-playground.js
@@ -11,11 +11,15 @@ const vertexShader = `
     varying float vRandom;
     varying vec2 vUv;
 
+
+
+
     void main() {
         float x = hoverState;
         vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-        modelPosition.z += x * sin(modelPosition.x * uFrequency.x + time) * 0.1;
-        modelPosition.z += x * sin(modelPosition.y * uFrequency.y + time) * 0.1;
+
+        modelPosition.z += x * tan(modelPosition.x * uFrequency.x + time) * 0.02;
+        modelPosition.z += x * tan(modelPosition.y * uFrequency.y + time) * 0.02;
 
         // modelPosition.z += aRandom * sin(time) * 0.1;
         vec4 viewPosition = viewMatrix * modelPosition;
@@ -32,18 +36,19 @@ const vertexShader = `
 
 const fragmentShader = `
     uniform float time;
-    uniform sampler2D uTexture;
+    uniform float hoverState;
+    // uniform sampler2D uTexture;
 
     precision mediump float;
 
     varying vec2 vUv;
-    varying float vRandom;
+    // varying float vRandom;
 
     void main() {
-
-        vec4 textureColor = texture2D(uTexture, vUv);
-        // gl_FragColor = vec4(vRandom * sin(time), vRandom * 0.5 * sin(time), 0.1, 1.0);
-        gl_FragColor = textureColor;
+        float x = hoverState;
+        // vec4 textureColor = texture2D(uTexture, vUv);
+        gl_FragColor = vec4(x * 0.83921568627,x *  0.63921568627,x *  0.27450980392, 1.0);
+        // gl_FragColor = textureColor;
     }
 `;
 
@@ -56,12 +61,13 @@ export default class Playground {
         // Sizing
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
+        this.cameraPos = 600;
 
         // Scene
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 1, 2000 );
-        this.camera.position.z = 2;
-
+        this.camera.position.z = this.cameraPos;
+        this.camera.fov = 2*Math.atan((this.height/2) / this.cameraPos ) * (180/Math.PI);
         // Renderer
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
         this.renderer.setSize( this.width, this.height );
@@ -72,11 +78,11 @@ export default class Playground {
         this.mouse = new THREE.Vector2();
         this.addObject();
         this.resize();
+        this.setPosition();
+        this.scaleObject();
         this.setupResize();
         // this.mouseMovement();
         this.render();
-
-
     }
 
     setupResize() {
@@ -88,15 +94,18 @@ export default class Playground {
         this.height = window.innerHeight;
         this.renderer.setSize( this.width, this.height );
         this.camera.aspect = this.width/this.height;
+        this.camera.fov = 2*Math.atan((window.innerHeight/2) / this.cameraPos ) * (180/Math.PI);
         this.camera.updateProjectionMatrix();
+        this.scaleObject();
+        this.setPosition();
     }
 
 
 
     addObject() {
-        this.texture = new THREE.TextureLoader();
-        const image = this.texture.load('/static/uploads/32.jpg');
-        this.geometry = new THREE.PlaneBufferGeometry(1,1,32,32);
+        // this.texture = new THREE.TextureLoader();
+        // const image = this.texture.load('/static/uploads/32.jpg');
+        this.geometry = new THREE.CircleGeometry(1,28);
         const count = this.geometry.attributes.position.count;
 
         const randoms = new Float32Array(count);
@@ -105,7 +114,7 @@ export default class Playground {
             randoms[i] = Math.random();
         }
 
-        this.texture.needsUpdate = true;
+        // this.texture.needsUpdate = true;
 
         this.geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1));
 
@@ -115,8 +124,8 @@ export default class Playground {
                 uImage: {value: 0},
                 uhover: {value: new THREE.Vector2(0.5,0.5)},
                 hoverState: {value: 0},
-                uFrequency: { value: new THREE.Vector2(10, 2)},
-                uTexture: { value: image }
+                uFrequency: { value: new THREE.Vector2(10, 2)}
+                // uTexture: { value: image }
             },
             side: THREE.DoubleSide,
             fragmentShader: fragmentShader,
@@ -145,7 +154,24 @@ export default class Playground {
         this.scene.add(this.mesh);
     }
 
+    scaleObject() {
+        for (let i = 0; i < this.scene.children.length; i++) {
+            const plane = this.scene.children[i];
+            const bounds = this.el.getBoundingClientRect();
 
+            console.log(bounds);
+            plane.scale.set(bounds.width, bounds.height,1);
+        }
+    }
+
+
+    setPosition() {
+        const bounds = this.el.getBoundingClientRect();
+
+        this.mesh.position.x = bounds.left - window.innerWidth / 2 + bounds.width / 2;
+        // get the scroll position, subtract the image top, add the height of window and half it and subtract the image height and half.
+        this.mesh.position.y = - bounds.top  + window.innerHeight / 2 - bounds.height / 2;
+    }
 
     mouseMovement() {
         window.addEventListener( 'mousemove', (event) => {
